2b9747555728683128baeaa76d451824a6481fb7
Mitovski <marvin.mit@tktk.ee>
2023-10-02 15:56:33 +0300

    add week4 code

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+int sum = 0;
+
+for(int i = 1; i < 11; i++) //i-- = i - 1
+{
+    Console.WriteLine($"i = {i + 1}");
+    Console.WriteLine($"Current toal: {sum}");
+    sum = sum + i; //sum += i;
+}
+
+Console.WriteLine($"Final total {sum}");
